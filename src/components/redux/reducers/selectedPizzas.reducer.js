975eec1c7c82f27b86780be3c61b4365269e6b3b
Travis Lang <tlang50@icloud.com>
2018-12-07 09:50:59 -0600

    add check for not allowing more than one of the same pizza

--- src/components/redux/reducers/selectedPizzas.reducer.js
+++ src/components/redux/reducers/selectedPizzas.reducer.js
@@ -1,6 +1,12 @@
 const selectedPizzas = ( state = [], action ) => {
     switch( action.type ) {
         case 'SELECT_PIZZA':
+            for (let pizza of state) {
+                if(action.payload.id == pizza.id) {
+                    alert('you can only have one of the same kind of pizza per order');
+                    return state;
+                }
+            }
             return [...state, action.payload];
         default:
             return state;
