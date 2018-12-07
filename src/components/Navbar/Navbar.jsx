dc907e143e10ec45b442590a84d64b08e24430e3
Anders Ryden <rydena021@gmail.com>
2018-12-07 14:11:32 -0600

    adds styling to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,6 +4,18 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
+import {HashRouter as Router, Link} from 'react-router-dom';
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import { createMuiTheme } from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors';
+
+const theme = createMuiTheme({
+  palette: {
+    secondary: {
+      main: '#ffffff'
+    }
+  }
+});
 
 class Navbar extends Component {
 
@@ -18,25 +30,29 @@ class Navbar extends Component {
         let navbarHtml;
         if (this.props.displayTotal) {
             navbarHtml =
-            <Grid xs={1}>
+            <Grid xs={2}>
                 <Typography className="total" variant="h6" color="inherit">
                 Total: {total}
                 </Typography>
             </Grid>
         }
         return (
-            <div>
+              <MuiThemeProvider theme={theme}>
+            <Router>
                 <AppBar className="navbar" position="static" color="primary">
                     <Toolbar>
-                        <Grid xs={11}>
-                        <Typography variant="h6" color="inherit">
-                            Prime Pizza
-                        </Typography>
+                        <Grid xs={10}>
+                          <Link to='/'>
+                            <Typography variant="h6" color="secondary">
+                              Prime Pizza
+                            </Typography>
+                          </Link>
                         </Grid>
                           {navbarHtml}
                     </Toolbar>
                 </AppBar>
-            </div>
+          </Router>
+              </MuiThemeProvider>
         )
     }
 }
